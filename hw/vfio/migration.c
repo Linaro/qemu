7f5bce7657e3179bc651a05cd985c9ac6fbc64a2
Joao Martins <joao.m.martins@oracle.com>
2023-06-22 22:48:45 +0100

    vfio/common: Block migration with vIOMMUs without address width limits
    Only block the case when the underlying vIOMMU model does not report any
    address space limits, in addition to DMA translation being off or no
    vIOMMU present. The limits are needed such that can define the IOVA limits
    that arm the device dirty tracker.
    
    Additionally, reword the migration blocker error message to clarify that
    we the configured vIOMMU does not support migration, as opposed to
    implying that just being there blocks migration.
    
    Signed-off-by: Joao Martins <joao.m.martins@oracle.com>

--- hw/vfio/migration.c
+++ hw/vfio/migration.c
@@ -1035,6 +1035,7 @@ static bool vfio_migration_iommu_tracking(VFIODevice *vbasedev)
 bool vfio_migration_realize(VFIODevice *vbasedev, Error **errp)
 {
     Error *err = NULL;
+    hwaddr max;
     int ret;
 
     if (vbasedev->enable_migration == ON_OFF_AUTO_OFF) {
@@ -1076,9 +1077,11 @@ bool vfio_migration_realize(VFIODevice *vbasedev, Error **errp)
     }
 
     if (vfio_viommu_preset(vbasedev) &&
-        !vfio_migration_iommu_tracking(vbasedev)) {
-        error_setg(&err, "%s: Migration is currently not supported "
-                   "with vIOMMU enabled", vbasedev->name);
+        !vfio_migration_iommu_tracking(vbasedev) &&
+        !vfio_viommu_get_max_iova(vbasedev->bcontainer, &max)) {
+        error_setg(&err,
+                   "%s: Migration with vIOMMU is currently not supported "
+                   "without vIOMMU address space boundaries", vbasedev->name);
         goto add_blocker;
     }
 
