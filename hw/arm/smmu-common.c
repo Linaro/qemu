2a2eb6453e5a67bba4b0431e5c336c57e6b8e831
Zhangfei Gao <zhangfei.gao@linaro.org>
2024-09-18 04:21:49 +0000

    update code

--- hw/arm/smmu-common.c
+++ hw/arm/smmu-common.c
@@ -25,6 +25,7 @@
 #include "qapi/error.h"
 #include "qemu/jhash.h"
 #include "qemu/module.h"
+#include <sys/ioctl.h>
 
 #include "qemu/error-report.h"
 #include "hw/arm/smmu-common.h"
@@ -927,7 +928,7 @@ static bool smmu_dev_attach_viommu(SMMUDevice *sdev,
     if (!iommufd_backend_alloc_hwpt(idev->iommufd, idev->devid, idev->ioas_id,
                                     IOMMU_HWPT_ALLOC_NEST_PARENT,
                                     IOMMU_HWPT_DATA_NONE, 0, NULL,
-                                    &s2_hwpt_id, errp)) {
+                                    &s2_hwpt_id, NULL, errp)) {
         error_setg(errp, "failed to allocate an S2 hwpt");
         return false;
     }
@@ -952,7 +953,7 @@ static bool smmu_dev_attach_viommu(SMMUDevice *sdev,
                                     viommu->core->viommu_id, 0,
                                     IOMMU_HWPT_DATA_ARM_SMMUV3,
                                     sizeof(abort_data), &abort_data,
-                                    &viommu->abort_hwpt_id, errp)) {
+                                    &viommu->abort_hwpt_id, NULL, errp)) {
         error_setg(errp, "failed to allocate an abort pagetable");
         goto free_viommu_core;
     }
@@ -961,7 +962,7 @@ static bool smmu_dev_attach_viommu(SMMUDevice *sdev,
                                     viommu->core->viommu_id, 0,
                                     IOMMU_HWPT_DATA_ARM_SMMUV3,
                                     sizeof(bypass_data), &bypass_data,
-                                    &viommu->bypass_hwpt_id, errp)) {
+                                    &viommu->bypass_hwpt_id, NULL, errp)) {
         error_setg(errp, "failed to allocate a bypass pagetable");
         goto free_abort_hwpt;
     }
@@ -1130,6 +1131,12 @@ void smmu_dev_uninstall_nested_ste(SMMUDevice *sdev, bool abort)
         hwpt_id = sdev->viommu->bypass_hwpt_id;
     }
 
+    if (s1_hwpt && s1_hwpt->out_fault_fd) {
+        s1_hwpt->exiting = true;
+        qemu_thread_join(&s1_hwpt->thread);
+        qemu_mutex_destroy(&s1_hwpt->fault_mutex);
+    }
+
     if (!host_iommu_device_iommufd_attach_hwpt(idev, hwpt_id, NULL)) {
         return;
     }
@@ -1140,7 +1147,7 @@ void smmu_dev_uninstall_nested_ste(SMMUDevice *sdev, bool abort)
 }
 
 int smmu_dev_install_nested_ste(SMMUDevice *sdev, uint32_t data_type,
-                                uint32_t data_len, void *data)
+                                uint32_t data_len, void *data, void * (*handler)(void *))
 {
     SMMUViommu *viommu = sdev->viommu;
     SMMUS1Hwpt *s1_hwpt = sdev->s1_hwpt;
@@ -1160,21 +1167,28 @@ int smmu_dev_install_nested_ste(SMMUDevice *sdev, uint32_t data_type,
     }
 
     s1_hwpt->smmu = sdev->smmu;
+    s1_hwpt->sdev = sdev;
     s1_hwpt->viommu = viommu;
     s1_hwpt->iommufd = idev->iommufd;
 
     if (!iommufd_backend_alloc_hwpt(idev->iommufd, idev->devid,
-                                    viommu->core->viommu_id, 0, data_type,
-                                    data_len, data, &s1_hwpt->hwpt_id, NULL)) {
+                                    viommu->core->viommu_id, IOMMU_HWPT_FAULT_ID_VALID, data_type,
+                                    data_len, data, &s1_hwpt->hwpt_id, &s1_hwpt->out_fault_fd, NULL)) {
         goto free;
     }
 
     if (!host_iommu_device_iommufd_attach_hwpt(idev, s1_hwpt->hwpt_id, NULL)) {
-        goto free_hwpt;
+	goto free_hwpt;
     }
 
     sdev->s1_hwpt = s1_hwpt;
 
+    if (s1_hwpt->out_fault_fd) {
+        qemu_mutex_init(&s1_hwpt->fault_mutex);
+        QTAILQ_INIT(&s1_hwpt->pageresp);
+        qemu_thread_create(&s1_hwpt->thread, "fault handler", handler,
+                           s1_hwpt, QEMU_THREAD_JOINABLE);
+    }
     return 0;
 free_hwpt:
     iommufd_backend_free_id(idev->iommufd, s1_hwpt->hwpt_id);
