ankita-nv/nicolinc-qemu

backends/iommufd: Introduce iommufd_viommu_alloc_eventq
Add a new helper for IOMMU_VEVENTQ_ALLOC ioctl to allocate a virtual event
queue (vEVENTQ) for a vIOMMU object.

Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>

backends/iommufd.c
@@ -441,6 +441,40 @@ int iommufd_viommu_invalidate_cache(IOMMUFDBackend *be, uint32_t viommu_id,
     return ret;
 }
 
+struct IOMMUFDVeventq *iommufd_viommu_alloc_eventq(IOMMUFDViommu *viommu,
+                                                   uint32_t type)
+{
+    int ret, fd = viommu->iommufd->fd;
+    struct IOMMUFDVeventq *veventq = g_malloc(sizeof(*veventq));
+    struct iommu_veventq_alloc alloc_veventq = {
+        .size = sizeof(alloc_veventq),
+        .flags = 0,
+        .type = type,
+        .viommu_id = viommu->viommu_id,
+    };
+
+    if (!veventq) {
+        error_report("failed to allocate veventq object");
+        return NULL;
+    }
+
+    ret = ioctl(fd, IOMMU_VEVENTQ_ALLOC, &alloc_veventq);
+
+    trace_iommufd_viommu_alloc_eventq(fd, viommu->viommu_id, type,
+                                      alloc_veventq.out_veventq_id,
+                                      alloc_veventq.out_veventq_fd, ret);
+    if (ret) {
+        error_report("IOMMU_VEVENTQ_ALLOC failed: %s", strerror(errno));
+        g_free(veventq);
+        return NULL;
+    }
+
+    veventq->viommu = viommu;
+    veventq->veventq_id = alloc_veventq.out_veventq_id;
+    veventq->veventq_fd = alloc_veventq.out_veventq_fd;
+    return veventq;
+}
+
 bool host_iommu_device_iommufd_attach_hwpt(HostIOMMUDeviceIOMMUFD *idev,
                                            uint32_t hwpt_id, Error **errp)
 {

include/sysemu/iommufd.h
@@ -43,6 +43,12 @@ typedef struct IOMMUFDViommu {
     uint32_t viommu_id;
 } IOMMUFDViommu;
 
+typedef struct IOMMUFDVeventq {
+    IOMMUFDViommu *viommu;
+    uint32_t veventq_id;
+    uint32_t veventq_fd;
+} IOMMUFDVeventq;
+
 bool iommufd_backend_connect(IOMMUFDBackend *be, Error **errp);
 void iommufd_backend_disconnect(IOMMUFDBackend *be);
 
@@ -77,6 +83,8 @@ struct IOMMUFDViommu *iommufd_backend_alloc_viommu(IOMMUFDBackend *be,
 int iommufd_viommu_invalidate_cache(IOMMUFDBackend *be, uint32_t viommu_id,
                                     uint32_t data_type, uint32_t entry_len,
                                     uint32_t *entry_num, void *data_ptr);
+struct IOMMUFDVeventq *iommufd_viommu_alloc_eventq(IOMMUFDViommu *viommu,
+                                                   uint32_t type);
 
 #define TYPE_HOST_IOMMU_DEVICE_IOMMUFD TYPE_HOST_IOMMU_DEVICE "-iommufd"
 OBJECT_DECLARE_TYPE(HostIOMMUDeviceIOMMUFD, HostIOMMUDeviceIOMMUFDClass,

Linaro/qemu

backends/iommufd.c
@@ -441,6 +441,42 @@ int iommufd_viommu_invalidate_cache(IOMMUFDBackend *be, uint32_t viommu_id,
     return ret;
 }
 
+struct IOMMUFDVeventq *iommufd_viommu_alloc_eventq(IOMMUFDViommu *viommu,
+                                                   uint32_t type,
+                                                   uint32_t depth)
+{
+    int ret, fd = viommu->iommufd->fd;
+    struct IOMMUFDVeventq *veventq = g_malloc(sizeof(*veventq));
+    struct iommu_veventq_alloc alloc_veventq = {
+        .size = sizeof(alloc_veventq),
+        .flags = 0,
+        .type = type,
+        .veventq_depth = depth,
+        .viommu_id = viommu->viommu_id,
+    };
+
+    if (!veventq) {
+        error_report("failed to allocate veventq object");
+        return NULL;
+    }
+
+    ret = ioctl(fd, IOMMU_VEVENTQ_ALLOC, &alloc_veventq);
+
+    trace_iommufd_viommu_alloc_eventq(fd, viommu->viommu_id, type,
+                                      alloc_veventq.out_veventq_id,
+                                      alloc_veventq.out_veventq_fd, ret);
+    if (ret) {
+        error_report("IOMMU_VEVENTQ_ALLOC failed: %s", strerror(errno));
+        g_free(veventq);
+        return NULL;
+    }
+
+    veventq->viommu = viommu;
+    veventq->veventq_id = alloc_veventq.out_veventq_id;
+    veventq->veventq_fd = alloc_veventq.out_veventq_fd;
+    return veventq;
+}
+
 bool host_iommu_device_iommufd_attach_hwpt(HostIOMMUDeviceIOMMUFD *idev,
                                            uint32_t hwpt_id, Error **errp)
 {

include/sysemu/iommufd.h
@@ -43,6 +43,12 @@ typedef struct IOMMUFDViommu {
     uint32_t viommu_id;
 } IOMMUFDViommu;
 
+typedef struct IOMMUFDVeventq {
+    IOMMUFDViommu *viommu;
+    uint32_t veventq_id;
+    uint32_t veventq_fd;
+} IOMMUFDVeventq;
+
 bool iommufd_backend_connect(IOMMUFDBackend *be, Error **errp);
 void iommufd_backend_disconnect(IOMMUFDBackend *be);
 
@@ -77,6 +83,9 @@ struct IOMMUFDViommu *iommufd_backend_alloc_viommu(IOMMUFDBackend *be,
 int iommufd_viommu_invalidate_cache(IOMMUFDBackend *be, uint32_t viommu_id,
                                     uint32_t data_type, uint32_t entry_len,
                                     uint32_t *entry_num, void *data_ptr);
+struct IOMMUFDVeventq *iommufd_viommu_alloc_eventq(IOMMUFDViommu *viommu,
+                                                   uint32_t type,
+                                                   uint32_t depth);
 
 #define TYPE_HOST_IOMMU_DEVICE_IOMMUFD TYPE_HOST_IOMMU_DEVICE "-iommufd"
 OBJECT_DECLARE_TYPE(HostIOMMUDeviceIOMMUFD, HostIOMMUDeviceIOMMUFDClass,