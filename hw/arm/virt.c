b01a4218d26534d88bf5967c0b668e262311cd58
Shameer Kolothum <skolothumtho@nvidia.com>
2025-09-22 16:03:45 +0100

    hw/arm/virt: Set PCI preserve_config for accel SMMUv3
    Introduce a new pci_preserve_config field in virt machine state which
    allows  the generation of DSM #5. This field is only set if accel SMMU
    is instantiated.
    
    In a subsequent patch, SMMUv3 accel mode will make use of IORT RMR nodes
    to enable nested translation of MSI doorbell addresses. IORT RMR requires
    _DSM #5 to be set for the PCI host bridge so that the Guest kernel
    preserves the PCI boot configuration.
    
    Reviewed-by: Jonathan Cameron <jonathan.cameron@huawei.com>
    Tested-by: Zhangfei Gao <zhangfei.gao@linaro.org>
    Reviewed-by: Eric Auger <eric.auger@redhat.com>
    Signed-off-by: Shameer Kolothum <skolothumtho@nvidia.com>

--- hw/arm/virt.c
+++ hw/arm/virt.c
@@ -3071,6 +3071,7 @@ static void virt_machine_device_pre_plug_cb(HotplugHandler *hotplug_dev,
             }
             object_property_set_uint(OBJECT(dev), "msi-gpa", db_start,
                                      &error_abort);
+            vms->pci_preserve_config = true;
         }
     }
 }
