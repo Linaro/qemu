5e75999112f655dc3731b31ba4ceafe39df0a997
Nicolin Chen <nicolinc@nvidia.com>
2025-10-13 01:59:30 +0000

    hw/arm/smmuv3: Check idr registers for STE_S1CDMAX and STE_S1STALLD
    With nested translation, the underlying HW could support those two fields.
    Allow them according to the updated idr registers after the hw_info ioctl.
    
    When substreams are enabled (S1CDMax != 0), S1DSS field determines
    the behavior of a transaction.
    
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>
    Signed-off-by: Shameer Kolothum <shameerali.kolothum.thodi@huawei.com>

--- hw/arm/smmuv3-internal.h
+++ hw/arm/smmuv3-internal.h
@@ -567,6 +567,7 @@ int smmu_find_ste(SMMUv3State *s, uint32_t sid, STE *ste, SMMUEventInfo *event);
 
 #define STE_S1FMT(x)       extract32((x)->word[0], 4 , 2)
 #define STE_S1CDMAX(x)     extract32((x)->word[1], 27, 5)
+#define STE_S1DSS(x)       extract32((x)->word[2], 0,  2)
 #define STE_S1STALLD(x)    extract32((x)->word[2], 27, 1)
 #define STE_EATS(x)        extract32((x)->word[2], 28, 2)
 #define STE_STRW(x)        extract32((x)->word[2], 30, 2)
